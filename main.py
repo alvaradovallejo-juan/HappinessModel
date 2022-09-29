02fb4aecd9ab5db0838b4a3627ebfa7e97abe75a
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2022-09-28 23:22:18 -0500

    Regressions Implementated

--- main.py
+++ main.py
@@ -17,3 +17,30 @@ if __name__ == "__main__":
 
     print(X.shape)
     print(Y.shape)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25)
+
+    modelLinear = LinearRegression().fit(X_train, Y_train)
+    Y_predict_linear = modelLinear.predict(X_test)
+
+    modelLasso = Lasso(alpha=0.02).fit(X_train, Y_train)
+    Y_predict_lasso = modelLasso.predict(X_test)
+
+    modelRidge = Ridge(alpha=1).fit(X_train, Y_train)
+    Y_predict_ridge = modelRidge.predict(X_test)
+
+    linear_loss = mean_squared_error(Y_test, Y_predict_linear)
+    print("Linear Loss: ", linear_loss)
+
+    lasso_loss = mean_squared_error(Y_test, Y_predict_lasso)
+    print("Lasso Loss: ", lasso_loss)
+
+    ridge_loss = mean_squared_error(Y_test, Y_predict_lasso)
+    print("Ridge Loss: ", ridge_loss)
+
+    print("=" * 32)
+    print("- Coef LASSO")
+    print(modelLasso.coef_)
+
+    print("- Coef RIDGE")
+    print(modelRidge.coef_)
